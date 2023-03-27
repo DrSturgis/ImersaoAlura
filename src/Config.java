271643ab2cc1c13f847f2f30b4433c1a14ffd756
Matheu <matheu.castelo@hotmail.com>
2023-03-27 12:13:56 -0300

    First commit aula 01.

---
+++ src/Config.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+
+    Properties props = new Properties();
+
+    public Config() {
+
+        try {
+            FileInputStream fis = new FileInputStream("config.properties");
+            props.load(fis);
+            fis.close();
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo de propriedades: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
